692c8ad866548672107844eeee3d80a18325ff06
Ravi Kumar Yadav <ravi.k.yadav108@gmail.com>
2016-02-03 14:57:00 +0530

    added 'middleware' which print type of request (GET, etc..) for each incoming request and pass it for further processing

--- express_router/server.js
+++ express_router/server.js
@@ -4,11 +4,24 @@ var app = express();
 var router = express.Router();
 var port = 3456;
 
+
+// adding middleware to print type of request (GET, POST, etc...)
+// being 'middleware', this will be executed for each request
+// next() allows us process the request further
+router.use(function(req, res, next){
+	console.log("/" + req.method);
+	next();
+});
+
+
 router.get("/", function(req, res){
 	res.json({"message": "Hello World"});
 });
 
+
+// "/api" is attached to each incoming request
 app.use("/api", router);
+
 
 app.listen(port, function(){
 	console.log("Live at Port " + port);

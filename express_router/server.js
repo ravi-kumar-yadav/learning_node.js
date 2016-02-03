67accf39507df73e6e59d57cd65cf0bbd325423c
Ravi Kumar Yadav <ravi.k.yadav108@gmail.com>
2016-02-03 15:13:01 +0530

    process request with 'id' at 'middleware' and 'normal' layer

--- express_router/server.js
+++ express_router/server.js
@@ -5,17 +5,37 @@ var router = express.Router();
 var port = 3456;
 
 
+// Executed first
 // adding middleware to print type of request (GET, POST, etc...)
 // being 'middleware', this will be executed for each request
 // next() allows us process the request further
+// 'middleware' can be used for logging, authentication, etc...
 router.use(function(req, res, next){
 	console.log("/" + req.method);
 	next();
 });
 
 
+// Executed second
+// accessing 'parameter' in 'middleware'
+// assuming the param name id 'id'
+router.use("/user/:id", function(req, res, next){
+	if (req.param.id == 0){
+		res.json({"message": "You must pass ID other than 0");
+	} else {
+		next();
+	}
+});
+
+
 router.get("/", function(req, res){
 	res.json({"message": "Hello World"});
+});
+
+
+// accessing 'id' in normal router
+router.get("/user/:id", function(req, res, next){
+	res.json({"message": "Hello " + req.param.id});
 });
 
 
